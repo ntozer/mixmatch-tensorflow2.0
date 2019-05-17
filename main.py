798d766fe342ec1f1b10467d33f6bd6347b086f1
Nathan Tozer <ntozer@unb.ca>
2019-05-16 22:13:04 -0300

    Implemented yaml config support, completed load_data function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
+import os
 
 import tensorflow as tf
+import yaml
 
 from preprocess import load_data
 
@@ -8,18 +10,39 @@ from preprocess import load_data
 def get_args():
     parser = argparse.ArgumentParser('parameters')
 
-    parser.add_argument('--dataset', type=str, default='SVHN',
-                        help='dataset used for training (e.g. CIFAR10, CIFAR100, SVHN, SVHN+EXTRA)')
-    parser.add_argument('--epochs', type=int, default=64, help="number of epochs, (default: 64)")
-    parser.add_argument('--batch-size',  type=int, default=256, help='examples per batch (default: 256)')
-    parser.add_argument("--learning-rate", type=float, default=1e-2, help="learning_rate, (default: 0.01)")
+    parser.add_argument('--seed', type=int, default=None, help='seed for repeatable results')
+    parser.add_argument('--dataset', type=str, default='svhn',
+                        help='dataset used for training (e.g. cifar10, cifar100, svhn, svhn+extra)')
+    parser.add_argument('--epochs', type=int, default=1024, help='number of epochs, (default: 64)')
+    parser.add_argument('--batch-size',  type=int, default=64, help='examples per batch (default: 256)')
+    parser.add_argument('--learning-rate', type=float, default=1e-2, help='learning_rate, (default: 0.01)')
+    parser.add_argument('--labelled-examples', type=int, default=4000, help='number labelled examples (default: 4000')
+    parser.add_argument('--T', type=float, default=0.5, help='temperature sharpening ratio (default: 0.5)')
+    parser.add_argument('--K', type=int, default=2, help='number of rounds of augmentation (default: 2)')
+    parser.add_argument('--alpha', type=float, default=0.75,
+                        help='param for sampling from Beta distribution (default: 0.75)')
+    parser.add_argument('--lambdaU', type=int, default=100, help='multiplier for unlabelled loss (default: 100)')
+
+    parser.add_argument('--config-path', type=str, default=None, help='path to yaml config file, overwrites args')
 
     return parser.parse_args()
 
 
+def load_config(args):
+    with open(args['config_path'], 'r') as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    for key in args.keys():
+        if key in config.keys():
+            args[key] = config[key]
+    return args
+
+
 def main():
-    args = get_args()
-    train, test = load_data(args)
+    args = vars(get_args())
+    if os.path.exists(args['config_path']):
+        args = load_config(args)
+
+    trainX, trainU, test = load_data(args)
     print(args)
 
 
--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
@@ -5,17 +6,79 @@ import tensorflow_datasets as tfds
 def load_data(args):
     train = None
     test = None
-    if args.dataset == 'SVHN':
-        train = tfds.load(name='svhn_cropped', split=tfds.Split.TRAIN)
-        test = tfds.load(name='svhn_cropped', split=tfds.Split.TEST)
-    elif args.dataset == 'SVHN+EXTRA':
-        train = tfds.load(name='svhn_cropped', split=tfds.Split.TRAIN)
-        extra = tfds.load(name='svhn_cropped', split=tfds.Split.EXTRA)
-        test = tfds.load(name='svhn_cropped', split=tfds.Split.TEST)
-    elif args.dataset == 'CIFAR10':
-        train = tfds.load(name='cifar10', split=tfds.Split.TRAIN)
-        test = tfds.load(name='cifar10', split=tfds.Split.TEST)
-    elif args.dataset == 'CIFAR100':
-        train = tfds.load(name='cifar100', split=tfds.Split.TRAIN)
-        test = tfds.load(name='cifar100', split=tfds.Split.TEST)
-    return train, test
+    if args['dataset'] == 'svhn':
+        dataset = tfds.load(name='svhn_cropped')
+        train = convert_to_numpy(dataset['train'])
+        test = convert_to_numpy(dataset['test'])
+        num_labels = 10
+
+    elif args['dataset'] == 'svhn+extra':
+        dataset = tfds.load(name='svhn_cropped')
+        train = convert_to_numpy(dataset['train'])
+        extra = convert_to_numpy(dataset['extra'])
+        for key in train.keys():
+            train[key] = np.concatenate([train[key], extra[key]], axis=0)
+        test = convert_to_numpy(dataset['test'])
+        num_labels = 10
+
+    elif args['dataset'] == 'cifar10':
+        dataset = tfds.load(name='cifar10')
+        train = convert_to_numpy(dataset['train'])
+        test = convert_to_numpy(dataset['test'])
+        num_labels = 10
+
+    elif args['dataset'] == 'cifar100':
+        dataset = tfds.load(name='cifar100')
+        train = convert_to_numpy(dataset['train'])
+        test = convert_to_numpy(dataset['test'])
+        num_labels = 100
+
+    train = convert_to_one_hot(train, num_labels)
+    test = convert_to_one_hot(test, num_labels)
+    trainX, trainU = generate_labelled_and_unlabelled_datasets(args, train)
+
+    return trainX, trainU, test
+
+
+def convert_to_numpy(dataset):
+    np_generator = tfds.as_numpy(dataset)
+    examples = {}
+    for example in np_generator:
+        for key in example:
+            try:
+                examples[key].append(example[key])
+            except KeyError:
+                examples[key] = [example[key]]
+    np_dataset = {}
+    for key in examples.keys():
+        np_dataset[key] = np.stack(examples[key])
+        # if key == 'label':
+        #     np_dataset[key] = np.expand_dims(np_dataset[key], axis=1)
+    return np_dataset
+
+
+def convert_to_one_hot(dataset, num_labels):
+    one_hot = np.zeros(shape=[len(dataset['label']), num_labels])
+    one_hot[np.arange(len(dataset['label'])), dataset['label']] = 1
+    dataset['label'] = one_hot
+    return dataset
+
+
+def generate_labelled_and_unlabelled_datasets(args, dataset):
+    dataset = unison_shuffle(args, dataset)
+    labelled = {}
+    unlabelled = {}
+    for key in dataset.keys():
+        labelled[key] = dataset[key][:args['labelled_examples']]
+        unlabelled[key] = dataset[key][args['labelled_examples']:]
+    unlabelled['label'] = np.zeros(shape=unlabelled['label'].shape)
+    return labelled, unlabelled
+
+
+def unison_shuffle(args, dataset):
+    assert len(set([len(dataset[key]) for key in dataset.keys()])) == 1
+    np.random.seed(args['seed'] if 'seed' in args.keys() else None)
+    p = np.random.permutation(len(dataset[list(dataset.keys())[0]]))
+    for key in dataset.keys():
+        dataset[key] = dataset[key][p]
+    return dataset
